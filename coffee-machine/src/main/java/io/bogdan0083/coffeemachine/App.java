e11b2b10a67c3ca42a9d24f4c891e7a652931d1a
Bogdan Dolin <d.bogdan@inbox.ru>
2020-08-21 14:06:25 +0700

    [cleanup] Remove todo

--- coffee-machine/src/main/java/io/bogdan0083/coffeemachine/App.java
+++ coffee-machine/src/main/java/io/bogdan0083/coffeemachine/App.java
@@ -4,7 +4,6 @@ import io.bogdan0083.coffeemachine.cli.CoffeeMachineCLI;
 
 public class App {
     public static void main(String[] args) {
-        // TODO: 21.08.2020 make coffee machine more composable
         CoffeeMachine machine = new CoffeeMachine.Builder().build();
         CoffeeMachineCLI cli = new CoffeeMachineCLI(machine);
         cli.run();
